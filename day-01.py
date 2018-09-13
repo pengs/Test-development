213411b81c9b51f903d499e47fab2a1a31d1c328
石鹏 <shipeng@uccc.cc>
2018-09-14 07:40:54 +0800

    新增

---
+++ day-01.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/13 23:29
+# @Author  : dapengchiji！！
+# @FileName: day-01.py
+
+import logging;logging.basicConfig(level=logging.INFO)
+import asyncio,os,json,time
+from  aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Hello Webapp!</h1>')
+
+@asyncio.coroutine
+def init(loop):
+    app=web.AppRunner(loop=loop)
+    app.router.add_route('GET','/',index)
+    srv=yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return  srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
