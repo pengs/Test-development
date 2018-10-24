e17e50573978d34b334dc520968276d28af7162e
开心就好 <wori21@qq.com>
2018-10-24 22:12:16 +0800

    Update 练习1.py
    修改符号

--- Python练习实例/练习1.py
+++ Python练习实例/练习1.py
@@ -91,7 +91,7 @@ if 1<=month<=12: # 判断月份
 		if month > 2:
 			if (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0)): # 判断闰年
 				d += 1 #如果是闰年 则多加一天
-		for i in range(1，12):
+		for i in range(1,12):
 			if i < month:
 				d += months[i]
 				i += 1
